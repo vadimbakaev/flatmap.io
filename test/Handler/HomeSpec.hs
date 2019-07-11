ef80e57e075c06977a5304400b7fb17aa765fc55
Vadim Bakaev <bakaev.vad@gmail.com>
2019-07-11 19:03:30 +0200

    remove unused fonts

--- test/Handler/HomeSpec.hs
+++ test/Handler/HomeSpec.hs
@@ -11,9 +11,6 @@ spec :: Spec
 spec =
   withApp $ do
     describe "Homepage" $
-        -- This is a simple example of using a database access in a test.  The
-        -- test will succeed for a fresh scaffolded site with an empty database,
-        -- but will fail on an existing database with a non-empty user table.
      do
       it "leaves the companies table empty" $ do
         get HomeR
