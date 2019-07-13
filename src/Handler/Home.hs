7d914d18e7037bbaf75ad3c5198c9ea1279ddebd
Vadim Bakaev <bakaev.vad@gmail.com>
2019-07-13 19:54:52 +0200

    close #30, add remote friendly company attribute

--- src/Handler/Home.hs
+++ src/Handler/Home.hs
@@ -13,7 +13,7 @@ import Import
 -- The majority of the code you will write in Yesod lives in these handler
 -- functions. You can spread them across multiple files if you are so
 -- inclined, or create a single monolithic file.
--- Let's handle IOException and decodeFail
+
 getHomeR :: Handler Html
 getHomeR = do
   companies <- runDB getAllCompanies
