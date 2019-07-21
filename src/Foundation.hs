6cf9ebdbb9a8b44c3edc0de65c83afdbac659d86
Vadim Bakaev <bakaev.vad@gmail.com>
2019-07-21 10:33:14 +0200

    fixed typo in mail scheme

--- src/Foundation.hs
+++ src/Foundation.hs
@@ -26,10 +26,7 @@ import Yesod.Core.Types (Logger)
 import qualified Yesod.Core.Unsafe as Unsafe
 import Yesod.Default.Util (addStaticContentExternal)
 
--- | The foundation datatype for your application. This can be a good place to
--- keep settings and values requiring initialization before your application
--- starts running, such as database connections. Every handler will have
--- access to the data present here.
+-- Every handler will have access to the data present here.
 data App =
   App
     { appSettings :: AppSettings
