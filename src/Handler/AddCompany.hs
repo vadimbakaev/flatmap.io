f3b9e1af3a3b9dda659d858451a16f4b776c30b3
Vadim Bakaev <bakaev.vad@gmail.com>
2019-07-31 22:52:44 +0200

    wip on add company forms

--- src/Handler/AddCompany.hs
+++ src/Handler/AddCompany.hs
@@ -11,43 +11,25 @@ module Handler.AddCompany where
 import Import
 
 getAddCompanyR :: Handler Html
-getAddCompanyR = do
-  (widget, enctype) <- generateFormPost newCompanyForm
+getAddCompanyR =
   defaultLayout $ do
     setTitle "Add Company"
     $(widgetFile "add-company-form")
 
 postAddCompanyR :: Handler Html
 postAddCompanyR = do
-  ((result, widget), enctype) <- runFormPost newCompanyForm
-  case result of
-    FormSuccess newCompany ->
-      defaultLayout $ do
-        setTitle $ toHtml $ mconcat ["Thank you for added ", name newCompany]
-        $(widgetFile "add-company-thank-you")
-    _ ->
-      defaultLayout
-        [whamlet|
-            <div .container>
-              <p>Invalid input, try again, please.
-              <form method=post action=@{AddCompanyR} enctype=#{enctype}>
-                 ^{widget}
-                 <button>Submit
-         |]
+  newCompany <-runInputPost $ NewCompany
+    <$> ireq textField "companyName"
+    <*> ireq textField "website"
+    <*> ireq textField "linkedin"
+    <*> ireq textField "github"
+    <*> ireq textField "address"
+    <*> ireq checkBoxField "startup"
+    <*> ireq checkBoxField "remote"
+  defaultLayout $ do
+    setTitle $ toHtml $ mconcat ["Thank you for added ", name newCompany]
+    $(widgetFile "add-company-thank-you")
 
-newCompanyForm :: Html -> MForm Handler (FormResult NewCompany, Widget)
-newCompanyForm =
-  renderTable $
-  NewCompany <$> areq textField "Company name" Nothing <*>
-  areq textField "Website" Nothing <*>
-  areq textField "LinkedIn" Nothing <*>
-  areq textField "GitHub" Nothing <*>
-  areq textField "Operative office address" Nothing <*>
-  areq boolField "Is it Startup?" (Just False) <*>
-  areq boolField "Is it Remote fiendly?" (Just False)
-
-
-  --TODO add languages, save newCompany on DB
 data NewCompany =
   NewCompany
     { name :: Text
