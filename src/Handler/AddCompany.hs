997d8a18cde2e9ca0e10465525e4376ccd237f40
Vadim Bakaev <bakaev.vad@gmail.com>
2019-08-01 23:20:05 +0200

    update company forms

--- src/Handler/AddCompany.hs
+++ src/Handler/AddCompany.hs
@@ -10,6 +10,21 @@ module Handler.AddCompany where
 
 import Import
 
+langs :: [Text]
+langs =
+  [ "Closure"
+  , "Elm"
+  , "Erlang"
+  , "F#"
+  , "Haskell"
+  , "Kotlin"
+  , "OCaml"
+  , "PureScript"
+  , "Racket"
+  , "Rust"
+  , "Scala"
+  ]
+
 getAddCompanyR :: Handler Html
 getAddCompanyR =
   defaultLayout $ do
@@ -18,14 +33,14 @@ getAddCompanyR =
 
 postAddCompanyR :: Handler Html
 postAddCompanyR = do
-  newCompany <-runInputPost $ NewCompany
-    <$> ireq textField "companyName"
-    <*> ireq textField "website"
-    <*> ireq textField "linkedin"
-    <*> ireq textField "github"
-    <*> ireq textField "address"
-    <*> ireq checkBoxField "startup"
-    <*> ireq checkBoxField "remote"
+  newCompany <-
+    runInputPost $
+    NewCompany <$> ireq textField "companyName" <*> ireq textField "website" <*>
+    ireq textField "linkedin" <*>
+    ireq textField "github" <*>
+    ireq textField "address" <*>
+    ireq checkBoxField "startup" <*>
+    ireq checkBoxField "remote"
   defaultLayout $ do
     setTitle $ toHtml $ mconcat ["Thank you for added ", name newCompany]
     $(widgetFile "add-company-thank-you")
