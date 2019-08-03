baf6b76cc69f6ef700004cebe630f07115e4eece
Vadim Bakaev <bakaev.vad@gmail.com>
2019-08-04 00:24:08 +0200

    add spam protection, update thank you page

--- src/Handler/AddCompany.hs
+++ src/Handler/AddCompany.hs
@@ -10,6 +10,9 @@ module Handler.AddCompany where
 
 import Import
 
+spamProtection :: Int
+spamProtection = 50
+
 getAddCompanyR :: Handler Html
 getAddCompanyR =
   defaultLayout $ do
@@ -20,9 +23,7 @@ postAddCompanyR :: Handler Html
 postAddCompanyR = do
   newCompany <-
     runInputPost $
-    NewCompany <$>
-    ireq textField "companyName" <*>
-    ireq textField "website" <*>
+    NewCompany <$> ireq textField "companyName" <*> ireq textField "website" <*>
     ireq textField "industry" <*>
     (Socials <$> ireq textField "github" <*> ireq textField "linkedin") <*>
     ireq textField "address" <*>
@@ -30,7 +31,8 @@ postAddCompanyR = do
     ireq checkBoxField "remote" <*>
     ((\ms -> [lang | (Just True, lang) <- ms `zip` langs]) <$>
      traverse (iopt checkBoxField) langs)
-  _ <- runDB $ insert newCompany
+  total <- runDB $ count ([] :: [Filter NewCompany])
+  when (total < spamProtection) $ void $ runDB $ insert newCompany
   defaultLayout $ do
     setTitle $
       toHtml $ mconcat ["Thank you for added ", newCompanyName newCompany]
