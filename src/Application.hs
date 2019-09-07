7e0296bbd109acec252bb432574162331bade570
Vadim Bakaev <bakaev.vad@gmail.com>
2019-09-07 22:12:40 +0200

    implement bookmarks saving

--- src/Application.hs
+++ src/Application.hs
@@ -53,7 +53,7 @@ import Handler.AddCompany
 import Handler.Common
 import Handler.Contacts
 import Handler.Home
-import Handler.Wishlist
+import Handler.Bookmarks
 
 -- This line actually creates our YesodDispatch instance. It is the second half
 -- of the call to mkYesodData which occurs in Foundation.hs. Please see the
