7f744412459ac7aa952b43a4b0a8d766f9260f14
Vadim Bakaev <bakaev.vad@gmail.com>
2019-10-20 16:35:35 +0200

    close #11, update file heades

--- src/Import/NoFoundation.hs
+++ src/Import/NoFoundation.hs
@@ -1,12 +1,30 @@
-{-# LANGUAGE CPP #-}
-module Import.NoFoundation
-    ( module Import
-    ) where
+{-
+    flatmap.io IT job search based on geo and technology.
+    Copyright (C) 2019 Vadim Bakaev
 
-import ClassyPrelude.Yesod   as Import
-import Model                 as Import
-import Settings              as Import
-import Settings.StaticFiles  as Import
-import Yesod.Auth            as Import
-import Yesod.Core.Types      as Import (loggerSet)
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-}
+{-# LANGUAGE CPP #-}
+
+module Import.NoFoundation
+  ( module Import
+  ) where
+
+import ClassyPrelude.Yesod as Import
+import Model as Import
+import Settings as Import
+import Settings.StaticFiles as Import
+import Yesod.Auth as Import
+import Yesod.Core.Types as Import (loggerSet)
 import Yesod.Default.Config2 as Import
--- src/Settings/StaticFiles.hs
+++ src/Settings/StaticFiles.hs
@@ -1,9 +1,27 @@
+{-
+    flatmap.io IT job search based on geo and technology.
+    Copyright (C) 2019 Vadim Bakaev
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+-}
 {-# LANGUAGE NoImplicitPrelude #-}
 {-# LANGUAGE OverloadedStrings #-}
-{-# LANGUAGE TemplateHaskell   #-}
+{-# LANGUAGE TemplateHaskell #-}
+
 module Settings.StaticFiles where
 
-import Settings     (appStaticDir, compileTimeAppSettings)
+import Settings (appStaticDir, compileTimeAppSettings)
 import Yesod.Static (staticFiles)
 
 -- This generates easy references to files in the static directory at compile time,
